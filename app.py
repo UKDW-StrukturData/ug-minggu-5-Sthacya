b31fb93707298f3b6c20633ae9c82797fdf3bdb8
Sthacya <sthacyalakamola@gmail.com>
2025-10-01 10:20:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 import csv
 import streamlit as st
+import io
+
 
 # --- Fungsi untuk load data ---
-def load_news(filename):
-    """Baca file news_data.csv ke list of dict"""
+def load_news(news_data):
+    try:
+        text = io.StringIO(news_data.getvalue().decode("utf-8"))
+    except:
+        text = io.StringIO(news_data)
+    reader = csv.DictReader(text, delimiter=',')
+    rows = []
+    for rows in reader:
+        rows.append({
+            "Headline": (row.get("Headline") or "").strip()
+        })
+    return rows
+"""Baca file news_data.csv ke list of dict"""
     # TODO: buka file CSV (filename) dan baca dengan csv.DictReader
     # kembalikan hasilnya dalam bentuk list
-    pass
+    
 
-def load_comments(filename):
+def load_comments(comment_news):
     """Baca file comment_news.csv ke list of dict"""
+    try:
+        text = io.StingIO(comment_news.getvalue().decode("utf-8"))
+    except:
+        text = io.StringIO(comment_news)
+    reader = csv.DictReader(text, delimiter=',')
+    rows = []
+    for rows in reader:
+        rows.append({
+            "IdBerita": (row.get("IdBerita") or "").strip(),
+            "Komentar": (row.get("Komentar") or "").strip(),
+            "Rating": (row.get("Rating") or "").strip(),
+        })
+    return rows
     # TODO: sama seperti load_news tapi untuk file komentar
-    pass
+
 
 # --- Fungsi untuk memproses data ---
 def process_data(news_list, comments_list):
@@ -21,7 +47,13 @@ def process_data(news_list, comments_list):
     Hasilnya list of dict.
     """
     # TODO: Buat dictionary untuk kumpulkan komentar per idBerita
-    comments_per_news = {}
+    # comments_per_news = {}
+    with open("comments_news.csv", "r") as datafile:
+        reader = csv.dictReader(datafile)
+        for row in reader:
+            print('')
+            pass
+        
 
     # TODO: isi comments_per_news dari comments_list
     # hint: per idBerita simpan ratings (list) dan count
@@ -53,6 +85,11 @@ def process_data(news_list, comments_list):
 def main():
     st.title("Analisis Sentimen & Popularitas Berita")
     st.write("Menampilkan ID, Headline, Rata-rata Rating, dan Jumlah Komentar, diurutkan dari rating tertinggi.")
+
+    coloum_config = {
+        "ID Berita": st.column_config.TextColumn("idBerita"),
+        "Headline": st.column_config.TextColumn("Headline")    
+    }
 
     # TODO: baca data CSV
     news_data = []     # ganti dengan pemanggilan load_news
